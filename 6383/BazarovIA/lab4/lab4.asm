902a71a2505486d49e6c1ad84d16685524529151
ilmurat <bazarov.ilmurat7@gmail.com>
2018-05-11 23:40:24 +0300

    asm fixed

--- 6383/BazarovIA/lab4/lab4.asm
+++ 6383/BazarovIA/lab4/lab4.asm
@@ -1,319 +1,305 @@
-ASSUME CS:CODE, DS:DATA, SS:MY_STACK
-;------------------------------------
-MY_STACK SEGMENT STACK 
-	DW 64 DUP(?)
-MY_STACK ENDS
-;------------------------------------
 CODE SEGMENT
-;------------------------------------
-INTERRUPTION PROC FAR
-	jmp FUNC_FOR_START
+ ASSUME CS:CODE, DS:DATA, ES:DATA, SS:ASTACK
+
+; ПРОЦЕДУРЫ
+;---------------------------------------
+; наш обработчик прерывания 
+ROUT PROC FAR
+	jmp nextJump
+	SIGNATURA dw 0ABCDh
+	KEEP_PSP dw 0 
+	KEEP_IP dw 0 
+	KEEP_CS dw 0
+	COUNT	dw 0 
+	VIVOD db 
+	KEEP_SS DW 0
+	KEEP_AX DW ?
+	KEEP_SP DW 0
+	INT_STACK DW 100 dup (?)
 	
-	;TMP DATA
-	PSP_ADDRESS_0 dw 0                            ;offset - 3
-	PSP_ADDRESS_1 dw 0	                          ;offset - 5
-	KEEP_CS dw 0                                  ;offset - 7
-	KEEP_IP dw 0                                  ;offset - 9
-	INTERRUPTION_SET dw 0FEDCh                 ;offset - 11
-	INT_COUNT db 'Interrupts call count: 0000  $' ;offset - 13
-
-FUNC_FOR_START:
-	push ax      
-	push bx
-	push cx
-	push dx
-
-	mov ah, 03h
-	mov bh, 00h
-	int 10h
-	push dx 
 	
-	mov ah, 02h
-	mov bh, 00h
-	mov dx, 0220h
-	int 10h
-
-	push si
-	push cx
-	push ds
-	mov ax, SEG INT_COUNT
-	mov ds, ax
-	mov si, offset INT_COUNT
-	add si, 1Ah
-
-	mov ah,[si]
-	inc ah
-	mov [si], ah
-	cmp ah, 3Ah
-	jne END_CALC
-	mov ah, 30h
-	mov [si], ah	
-
-	mov bh, [si - 1] 
-	inc bh
-	mov [si - 1], bh
-	cmp bh, 3Ah                    
-	jne END_CALC
-	mov bh, 30h
-	mov [si - 1], bh
-
-	mov ch, [si - 2]
-	inc ch
-	mov [si - 2], ch
-	cmp ch, 3Ah
-	jne END_CALC
-	mov ch, 30h
-	mov [si - 2], ch
-
-	mov dh, [si - 3]
-	inc dh
-	mov [si - 3], dh
-	cmp dh, 3Ah
-	jne END_CALC
-	mov dh, 30h
-	mov [si - 3],dh
+	nextJump:
+	mov KEEP_SS,SS
+	mov KEEP_SP,SP
+	mov KEEP_AX,AX
+	mov AX,seg INT_STACK
+	mov SS,AX
+	mov SP,0
+	mov AX,KEEP_AX
 	
-END_CALC:
-    pop ds
-    pop cx
-	pop si
-	
+	push ax
+	push bp
 	push es
-		push bp
-			mov ax, SEG INT_COUNT
-			mov es, ax
-			mov ax, offset INT_COUNT
-			mov bp, ax
-			mov ah, 13h
-			mov al, 00h
-			mov cx, 1Dh
-			mov bh, 0
-			int 10h
-		pop bp
-	pop es
+	push ds
+	push dx
+	push di
 	
+	mov ax,cs
+	mov ds,ax 
+	mov es,ax 
+	mov ax,CS:COUNT
+	add ax,1
+	mov CS:COUNT,ax
+	mov di,offset vivod+34
+	call WRD_TO_HEX
+	mov bp,offset vivod
+	call outputBP
+	
+	; восстанавливаем регистры:
+	pop di
 	pop dx
-	mov ah, 02h
-	mov bh, 0h
-	int 10h
-
-	pop dx
-	pop cx
-	pop bx
-	pop ax     
-
+	pop ds
+	pop es
+	pop bp
+	mov al,20h
+	out 20h,al
+	pop ax
+	mov AX,KEEP_SS
+	mov SS,AX
+	mov AX,KEEP_AX
+	mov SP,KEEP_SP
 	iret
-INTERRUPTION ENDP
-;------------------------------------
-NEED_MEM_AREA PROC
-NEED_MEM_AREA ENDP
-;------------------------------------
-IS_INTERRUPTION_SET PROC NEAR
-	push bx
-	push dx
-	push es
-
-	mov ah, 35h
-	mov al, 1Ch
-	int 21h
-
-	mov dx, es:[bx + 11]
-	cmp dx, 0FEDCh
-	je INT_IS_SET
-	mov al, 00h
-	jmp POP_REG
-
-INT_IS_SET:
-	mov al, 01h
-	jmp POP_REG
-
-POP_REG:
-	pop es
-	pop dx
-	pop bx
-
-	ret
-IS_INTERRUPTION_SET ENDP
-;------------------------------------
-CHECK_COMMAND_PROMT PROC NEAR
-	push es
 	
-	mov ax, PSP_ADDRESS_0
-	mov es, ax
-
-	mov bx, 0082h
-
-	mov al, es:[bx]
-	inc bx
-	cmp al, '/'
-	jne NULL_CMD
-
-	mov al, es:[bx]
-	inc bx
-	cmp al, 'u'
-	jne NULL_CMD
-
-	mov al, es:[bx]
-	inc bx
-	cmp al, 'n'
-	jne NULL_CMD
-
-	mov al, 0001h
-NULL_CMD:
-	pop es
-
+ROUT ENDP 
+; --------------------------------------
+TETR_TO_HEX PROC near
+	and AL,0Fh
+	cmp AL,09
+	jbe NEXT
+	add AL,07
+NEXT: add AL,30h
 	ret
-CHECK_COMMAND_PROMT ENDP
-;------------------------------------
-LOAD_INTERRUPTION PROC NEAR
+TETR_TO_HEX ENDP
+;---------------------------------------
+BYTE_TO_HEX PROC near
+	push CX
+	mov AH,AL
+	call TETR_TO_HEX
+	xchg AL,AH
+	mov CL,4
+	shr AL,CL
+	call TETR_TO_HEX
+	pop CX
+	ret
+BYTE_TO_HEX ENDP
+;---------------------------------------
+; перевод в 16с/с 16-ти разрядного числа
+; в AX - число, DI - адрес последнего символа
+WRD_TO_HEX PROC near
+	push BX
+	mov BH,AH
+	call BYTE_TO_HEX
+	mov [DI],AH
+	dec DI
+	mov [DI],AL
+	dec DI
+	mov AL,BH
+	call BYTE_TO_HEX
+	mov [DI],AH
+	dec DI
+	mov [DI],AL
+	pop BX
+	ret
+WRD_TO_HEX ENDP
+;---------------------------------------
+; Функция вывода строки из BP
+outputBP PROC near
 	push ax
 	push bx
 	push dx
-	push es
-
-	mov ah, 35h
-	mov al, 1Ch
+	push cx
+	mov ah,13h
+	mov al,0
+	mov bl,09h
+	mov bh,0
+	mov dh,4
+	mov dl,22
+	mov cx,35
+	int 10h  
+	pop cx
+	pop dx
+	pop bx
+	pop ax
+	ret
+outputBP ENDP
+LAST_BYTE:
+;---------------------------------------
+PRINT PROC
+	push ax
+	mov ah,09h
 	int 21h
-
-	mov KEEP_IP, bx
-	mov KEEP_CS, es
-
+	pop ax
+	ret
+PRINT ENDP
+;---------------------------------------
+; проверка, установлен ли наш обработчик прерывания:
+PROV_ROUT PROC
+	mov ah,35h
+	mov al,1ch
+	int 21h ; получили в ES:BX адрес обработчика прерывания
+	mov si,offset SIGNATURA
+	sub si,offset ROUT ; в SI - смещение сигнатуры относительно начала обработчика
+	mov ax,0ABCDh
+	cmp ax,ES:[BX+SI] ; сравниваем сигнатуры
+	je ROUT_EST
+		call SET_ROUT
+		jmp PROV_KONEC
+	ROUT_EST:
+		call DEL_ROUT
+	PROV_KONEC:
+	ret
+PROV_ROUT ENDP
+;---------------------------------------
+; установка нашего обработчика:
+SET_ROUT PROC
+	mov ax,KEEP_PSP 
+	mov es,ax ; кладём в es PSP нашей програмы
+	cmp byte ptr es:[80h],0
+		je UST
+	cmp byte ptr es:[82h],'/'
+		jne UST
+	cmp byte ptr es:[83h],'u'
+		jne UST
+	cmp byte ptr es:[84h],'n'
+		jne UST
+	
+	mov dx,offset PRER_NE_SET_VIVOD
+	call PRINT
+	ret
+	
+	UST:
+	; сохраняем стандартный обработчик:
+	call SAVE_STAND	
+	
+	mov dx,offset PRER_SET_VIVOD
+	call PRINT
+	
 	push ds
-		mov dx, offset INTERRUPTION
-		mov ax, seg INTERRUPTION
-		mov ds, ax
-
-		mov ah, 25h
-		mov al, 1Ch
-		int 21h
+	; кладём в ds:dx адрес нашего обработчика:
+	mov dx,offset ROUT
+	mov ax,seg ROUT
+	mov ds,ax
+	
+	; меняем адрес обработчика прерывания 1Ch:
+	mov ah,25h
+	mov al,1ch
+	int 21h
 	pop ds
-
-	mov dx, offset M_INT_ISLOADED0
-	call PRINT_STRING
-
+	
+	; оставляем программу резидентно:
+	mov dx,offset LAST_BYTE
+	mov cl,4
+	shr dx,cl ; делим dx на 16
+	add dx,1
+	add dx,20h
+		
+	xor AL,AL
+	mov ah,31h
+	int 21h ; оставляем наш обработчик в памяти
+		
+	xor AL,AL
+	mov AH,4Ch
+	int 21H
+SET_ROUT ENDP
+;---------------------------------------
+; удаление нашего обработчика:
+DEL_ROUT PROC
+	push dx
+	push ax
+	push ds
+	push es
+	
+	
+	mov ax,KEEP_PSP 
+	mov es,ax ; кладём в es PSP нашей програмы
+	cmp byte ptr es:[80h],0
+		je UDAL_KONEC
+	cmp byte ptr es:[82h],'/'
+		jne UDAL_KONEC
+	cmp byte ptr es:[83h],'u'
+		jne UDAL_KONEC
+	cmp byte ptr es:[84h],'n'
+		jne UDAL_KONEC
+	
+	mov dx,offset PRER_DEL_VIVOD
+	call PRINT
+	
+	mov ah,35h
+	mov al,1ch
+	int 21h ; получили в ES:BX адрес нашего обработчика
+	mov si,offset KEEP_IP
+	sub si,offset ROUT
+	
+	; возвращаем стандартный обработчик:
+	mov dx,es:[bx+si]
+	mov ax,es:[bx+si+2]
+	mov ds,ax
+	mov ah,25h
+	mov al,1ch
+	int 21h
+	
+	; удаляем из памяти наш обработчик:
+	mov ax,es:[bx+si-2] ; получили psp нашего обработчика
+	mov es,ax
+	mov ax,es:[2ch] ; получили сегментный адрес среды
+	push es
+	mov es,ax
+	mov ah,49h
+	int 21h
 	pop es
-	pop dx
-	pop bx
-	pop ax
+	mov ah,49h
+	int 21h
 
+	jmp UDAL_KONEC2
+	
+	UDAL_KONEC:
+	mov dx,offset PRER_UZHE_SET_VIVOD
+	call PRINT
+	UDAL_KONEC2:
+	
+	pop es
+	pop ds
+	pop ax
+	pop dx
 	ret
-LOAD_INTERRUPTION ENDP
-;------------------------------------
-UNLOAD_INTERRUPTION PROC NEAR
+DEL_ROUT ENDP
+;---------------------------------------
+; сохранение адреса стандартного обработчика в KEEP_IP и KEEP_CS:
+SAVE_STAND PROC
 	push ax
 	push bx
-	push dx
 	push es
-
-	mov ah, 35h
-	mov al, 1Ch
-	int 21h
-
-	cli
-	push ds            
-		mov dx, es:[bx + 9]   
-		mov ax, es:[bx + 7]   
-		
-		mov ds, ax
-		mov ah, 25h
-		mov al, 1Ch
-		int 21h
-	pop ds
-	sti
-
-	mov dx, offset M_INT_RESTORED
-	call PRINT_STRING
-
-	push es	
-		mov cx, es:[bx + 3]
-		mov es, cx
-		mov ah, 49h
-		int 21h
+	mov ah,35h
+	mov al,1ch
+	int 21h ; получили в ES:BX адрес обработчика прерывания
+	mov KEEP_CS, ES
+	mov KEEP_IP, BX
 	pop es
-	
-	mov cx, es:[bx + 5]
-	mov es, cx
-	int 21h
-
-	pop es
-	pop dx
 	pop bx
 	pop ax
-	
 	ret
-UNLOAD_INTERRUPTION ENDP
-;------------------------------------
-PRINT_STRING PROC NEAR
-	push ax
-	mov ah, 09h
-	int	21h
-	pop ax
-	ret
-PRINT_STRING ENDP
-;------------------------------------
-MAIN_PROGRAM PROC FAR
-	mov bx, 02Ch
-	mov ax, [bx]
-	mov PSP_ADDRESS_1, ax
-	mov PSP_ADDRESS_0, ds  
-	sub ax, ax    
-	xor bx, bx
-
-	mov ax, DATA  
-	mov ds, ax    
-
-	call CHECK_COMMAND_PROMT   ;Загрузка или выгрузка(проверка параметра)
-	cmp al, 01h
-	je UNLOAD_START
-
-	call IS_INTERRUPTION_SET   ;Установлен ли разработанный вектор прерывания
-	cmp al, 01h
-	jne INTERRUPTI0N_IS_NOT_LOADED
-	
-	mov dx, offset M_INT_ISLOADED	;Уже установлен(выход с сообщение)
-	call PRINT_STRING
-	jmp EXIT_PROGRAM
-       
-	mov ah,4Ch
-	int 21h
-
-INTERRUPTI0N_IS_NOT_LOADED:
-	call LOAD_INTERRUPTION
-	
-	mov dx, offset NEED_MEM_AREA
-	mov cl, 04h
-	shr dx, cl
-	add dx, 1Bh
-
-	mov ax, 3100h
-	int 21h
-         
-UNLOAD_START:
-	call IS_INTERRUPTION_SET
-	cmp al, 00h
-	je INT_IS_NOT_SET
-	call UNLOAD_INTERRUPTION
-	jmp EXIT_PROGRAM
-
-INT_IS_NOT_SET:
-	mov dx, offset M_INT_NOT_SET
-	call PRINT_STRING
-    jmp EXIT_PROGRAM
-	
-EXIT_PROGRAM:
-	mov ah, 4Ch
-	int 21h
-MAIN_PROGRAM ENDP
-;------------------------------------
+SAVE_STAND ENDP
+;---------------------------------------
+BEGIN:
+	mov ax,DATA
+	mov ds,ax
+	mov KEEP_PSP, es
+	call PROV_ROUT
+	xor AL,AL
+	mov AH,4Ch
+	int 21H
 CODE ENDS
-;------------------------------------
-DATA SEGMENT
-	;messages
-	M_INT_NOT_SET db "Interruption didnt load!", 0dh, 0ah, '$'
-	M_INT_RESTORED db "Interruption was restored!", 0dh, 0ah, '$'
-	M_INT_ISLOADED db "Interruption already load!", 0dh, 0ah, '$'
-	M_INT_ISLOADED0 db "Interuption is loading now!", 0dh, 0ah, '$'
-DATA ENDS
-;------------------------------------
-END MAIN_PROGRAM
 
+; ДАННЫЕ
+DATA SEGMENT
+	PRER_SET_VIVOD db 'Setup interrupt','$'
+	PRER_DEL_VIVOD db 'Uninstall interrupt',0DH,0AH,'$'
+	PRER_UZHE_SET_VIVOD db 'Interrupt is already set',0DH,0AH,'$'
+	PRER_NE_SET_VIVOD db 'Interrupt is not set',0DH,0AH,'$'
+	STRENDL db 0DH,0AH,'$'
+DATA ENDS
+; СТЕК
+ASTACK SEGMENT STACK
+	dw 100h dup (?)
+ASTACK ENDS
+ END BEGIN
